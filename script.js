060827b4814e5da367b0ed44a709cb846d001463
Pulak Pramanik <131440933+Pulakkantipramanik@users.noreply.github.com>
2025-12-19 06:21:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const canvas = 
     document.querySelector("canvas"),
 toolBtns = 
@@ -226,6 +224,7 @@ canvas.addEventListener("mouseup", () => isDrawing = false);
 
 // Event listener for the undo button
 undoBtn.addEventListener("click", undo);
+
 
 
 
